f3f6f4cbb34ef735e5b30c52b653fe25712106df
mk13 <97834754+karameh13@users.noreply.github.com>
2023-06-21 15:46:27 +0200

    Add files via upload

---
+++ catkin_ws/updated/MotorDriver.cpp
@@ -0,0 +1,96 @@
+#include "MotorDriver.h"
+#include <chrono>
+#include <thread>
+
+using namespace std::this_thread; 
+using namespace std::chrono; 
+
+MD::Motor::Motor(int directionPin, int pwmPin, double minSpeed, double maxSpeed) {
+    m_directionPin = directionPin;
+    m_pwmPin = pwmPin;
+    m_minSpeed = minSpeed;
+    m_maxSpeed = maxSpeed;
+    m_speed = 0.0;
+    m_direction = 0;
+
+	// Pin configurations for JetsonGPIO
+	GPIO::setmode(GPIO::BOARD); // GPIO::BCM or GPIO::BOARD
+	GPIO::setup(32, GPIO::OUT, GPIO::HIGH); // Left motor
+	GPIO::setup(33, GPIO::OUT, GPIO::HIGH); // Right motor
+	GPIO::setup(31, GPIO::OUT, GPIO::HIGH); // Left direction
+	GPIO::setup(35, GPIO::OUT, GPIO::HIGH); // Right direction  
+}
+
+// --- Pwm pin ---.
+int MD::Motor::getPwmPin(){
+    return m_pwmPin;
+}
+// --- End of Pwm pin ---
+
+// --- Wheel direction ---
+void MD::Motor::setDirection(double speed) {
+    if (speed >= 0) {
+    	//sleep_for(nanoseconds(10000000));
+        m_direction = true;
+    } else if (speed < 0) {
+       	//sleep_for(nanoseconds(10000000));
+        m_direction = false; 
+    }
+}
+
+bool MD::Motor::getDirection() {
+	return m_direction;
+}
+
+int MD::Motor::getDirectionPin(){
+    return m_directionPin;
+}
+// --- End of wheel direction ---
+
+// --- Control of motor actions ---
+void MD::Motor::setSpeed(double speed) {
+    if (speed <= m_minSpeed) {
+    	//sleep_for(nanoseconds(10000000));
+        m_speed = speed*-1;
+    } else if (speed < m_maxSpeed*-1) {
+    	//sleep_for(nanoseconds(10000000));
+    	m_speed = m_maxSpeed;
+    } else if (speed >= m_maxSpeed) {
+    	//sleep_for(nanoseconds(10000000));
+        m_speed = m_maxSpeed;
+    } else {
+    	//sleep_for(nanoseconds(10000000));
+   		m_speed = speed;
+    	/*while(m_speed != speed)
+    	{
+			if(m_speed < speed)
+    	  		m_speed ++;
+    	  		
+    	  	if(m_speed > speed)
+    	  		m_speed --;
+    	//m_speed = speed;
+    	}*/
+    }
+}
+
+double MD::Motor::getSpeed() {
+    return m_speed;
+}
+
+void MD::Motor::setMinSpeed(double minSpeed) {
+    m_minSpeed = minSpeed;
+}
+
+// int Motor::getMinSpeed() {return 0;}
+
+void MD::Motor::setMaxSpeed(double maxSpeed) {
+    m_maxSpeed = maxSpeed;
+}
+
+// int Motor::getMaxSpeed() {return 0;}
+
+void MD::Motor::stop() {
+    m_speed = 0.0;
+}
+// --- End of control of motor actions ---
+
---
+++ catkin_ws/updated/XBoxController.cpp
@@ -0,0 +1,68 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <ostream>
+
+#define B_BUTTON 1
+
+#define ANGULAR_VEL 0
+#define JOY_AXIS_RT 5
+#define JOY_AXIS_LT 2
+
+class XBoxController
+{
+public:
+  XBoxController();
+
+private:
+  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+  ros::NodeHandle nh;
+
+  int linear, angular;
+  int b_button;
+  double l_scale, a_scale;
+  ros::Publisher vel_pub;
+  ros::Subscriber joy_sub;
+};
+
+
+XBoxController::XBoxController():
+  linear(JOY_AXIS_RT),
+  angular(ANGULAR_VEL),
+  b_button(B_BUTTON)
+{
+
+  nh.param("axis_linear", linear, linear);
+  nh.param("axis_angular", angular, angular);
+  nh.param("scale_angular", a_scale, a_scale);
+  nh.param("scale_linear", l_scale, l_scale);
+  nh.param("buttons", b_button, b_button);
+
+  vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+
+  joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &XBoxController::joyCallback, this);
+}
+
+void XBoxController::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  geometry_msgs::Twist twist;
+  twist.angular.z = a_scale*joy->axes[ANGULAR_VEL];
+  twist.linear.x = l_scale*joy->axes[JOY_AXIS_RT];
+  twist.linear.x = (1+ (twist.linear.x*-1))/2;
+  auto button = joy->buttons[B_BUTTON];
+  if(button == 1){
+  	twist.linear.x = 0;
+  	twist.angular.z = 0;
+  }
+  vel_pub.publish(twist);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "xbox_controller");
+  XBoxController xbox_controller;
+
+  ros::spin();
+}
+
